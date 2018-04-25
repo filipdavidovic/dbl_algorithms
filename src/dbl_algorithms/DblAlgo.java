a511d3bedeb9b7c49c4c87b514c3252da552bc68
Filip Davidovic <filip.davidovic.fd@gmail.com>
2018-04-25 12:20:38 +0200

    first prototype v1

--- src/dbl_algorithms/DblAlgo.java
+++ src/dbl_algorithms/DblAlgo.java
@@ -1,21 +1,77 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dbl_algorithms;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
- *
- * @author Veronika
+ * Main class of the program. Execute the program by running this class and providing appropriate input.
  */
 public class DblAlgo {
 
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
     /**
-     * @param args the command line arguments
+     * Method that places given rectangles one next to each other in order they are provided.
+     * The width of this container will be the sum of all rectangles' widths (x-coordinate).
+     *
+     * @param rectangles - rectangles to be ordered.
+     * @return - lower x and lower y coordinates of each of the rectangles.
      */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private int[][] noSort(int[][] rectangles) {
+        int[][] ret = new int[rectangles.length][2];
+
+        int sum = 0;
+
+        for(int i = 0; i < rectangles.length; i++) {
+            ret[i][0] = sum;
+            ret[i][1] = 0;
+            sum += rectangles[i][0];
+        }
+
+        return ret;
     }
-    
+
+    /**
+     * Method to collect input and run appropriate an algoritm based on it.
+     *
+     * @throws IOException
+     */
+    private void run() throws IOException {
+        /* input */
+        // container height: {free, fixed}
+        String containerHeight = br.readLine().split("container height: ")[1]; // {free, fixed}
+
+        // rotations allowed: {yes, no}
+        String rotationsAllowed = br.readLine().split("rotations allowed: ")[1]; // {yes, no}
+
+        // number of rectangles: n
+        int numberOfRectangles = Integer.parseInt(br.readLine().split("number of rectangles: ")[1]); // n
+
+        // rectangle input
+        int[][] rectangles = new int [numberOfRectangles][2];
+
+        for(int i = 0; i < numberOfRectangles; i++) {
+            String[] input = br.readLine().split(" ");
+            rectangles[i][0] = Integer.parseInt(input[0]);
+            rectangles[i][1] = Integer.parseInt(input[1]);
+        }
+
+        /* output */
+        System.out.println("container height: " + containerHeight);
+        System.out.println("rotations allowed: " + rotationsAllowed);
+        System.out.println("number of rectangles: " + numberOfRectangles);
+        for(int i = 0; i < numberOfRectangles; i++) {
+            System.out.println(rectangles[i][0] + " " + rectangles[i][1]);
+        }
+        System.out.println("placement of rectangles");
+        int[][] placement = noSort(rectangles);
+        for(int i = 0; i < numberOfRectangles; i++) {
+            System.out.println("no " + placement[i][0] + " " + placement[i][1]);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Main().run();
+    }
 }
