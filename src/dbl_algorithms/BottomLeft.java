286e95c8ae230f9a254a50b23d9c75e9e8cd95b3
Filip Davidovic <filip.davidovic.fd@gmail.com>
2018-05-02 13:04:33 +0200

    basic bottom-left working

--- src/dbl_algorithms/BottomLeft.java
+++ src/dbl_algorithms/BottomLeft.java
@@ -22,17 +22,16 @@ public class BottomLeft extends PackingStrategy {
 
         State s = new State(rectangles.length);
 
-        final int frameWidth = 100;
+        final int frameWidth = 20;
         int height = 0;
 
         for(int i = 0; i < rectangles.length; i++) {
-            int sx = height + rectangles[i].height;
-            int sy = frameWidth - rectangles[i].width;
+            int sx = frameWidth - rectangles[i].width;
 
             int maxHeight = 0;
             for(int j = 0; j < i; j++) {
                 // check that the rectangle is below the current position and that its x is greater than the current maxHeight
-                if(rectangles[j].blx <= sx && rectangles[j].blx + rectangles[j].width > sx && rectangles[j].bly + rectangles[j].height > maxHeight) {
+                if(rectangles[j].blx < sx + rectangles[i].width && rectangles[j].blx + rectangles[j].width > sx && rectangles[j].bly + rectangles[j].height > maxHeight) {
                     maxHeight = rectangles[j].bly + rectangles[j].height;
                 }
             }
@@ -40,7 +39,7 @@ public class BottomLeft extends PackingStrategy {
             int maxWidth = 0;
             for(int j = 0; j < i; j++) {
                 // check that the rectangle is to the left of the current position and that its y is greater than the current maxWidth
-                if(rectangles[j].bly <= sy && rectangles[j].bly + rectangles[j].height > sy && rectangles[j].blx + rectangles[j].width > maxWidth) {
+                if(rectangles[j].bly < maxHeight + rectangles[i].height && rectangles[j].bly + rectangles[j].height > maxHeight && rectangles[j].blx + rectangles[j].width > maxWidth) {
                     maxWidth = rectangles[j].blx + rectangles[j].width;
                 }
             }
@@ -48,6 +47,11 @@ public class BottomLeft extends PackingStrategy {
             rectangles[i].setPosition(maxWidth, maxHeight);
 
             s.addRectangle(rectangles[i]);
+
+            // update the height of the frame
+            if(maxHeight + rectangles[i].height > height) {
+                height = maxHeight + rectangles[i].height;
+            }
         }
 
         return s;
